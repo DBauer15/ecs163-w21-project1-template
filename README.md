f6f066a0d267a6bdf6859d5570f1d7fe08e6723c
David Bauer <DBauer15@users.noreply.github.com>
2020-12-29 10:58:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ECS163 WQ2021 - Homework Assignment 4
+
+
+## Start here
+Run `npm install` to install missing node packages and run the project.
+
+Run `npm run serve` to serve the project locally.
+
+## Documentation
+If the project requires additional setup or if you chose to use raw HTML instead of VueJS:
+
+*Provide your documentation here*
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- This file will be used to layout and connect your views -->
+<template>
+  <div id="app">
+      <View1 />
+      <View2 />
+  </div>
+</template>
+
+<script>
+import View1 from './components/View1.vue'
+import View2 from './components/View2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    View1,
+    View2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
